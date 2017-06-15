e84b8c67dff8b7dff225393c835120e8f4372727
Matin <matin.deutsch@gmail.com>
2017-06-15 10:17:27 +0200

    Add files via upload
    price + 19% Tax

---
+++ Steuer-19%.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Steuer {
+
+   public static void main(String[] args){
+   
+        Scanner sc = new Scanner(System.in);
+         System.out.println("Bitte geben Sie einen Betrag");
+         
+        double betrag = sc.nextDouble();
+        
+        
+        double Summe = betrag* 1.19;
+        
+        System.out.println(Summe);
+         
+   
+   
+   
+   
+   
+   
+   
+   
+     }
+
+  
+  }
